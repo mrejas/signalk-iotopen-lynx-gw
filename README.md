7a92bf3047715c30c5ab3f1ad3fe178b5720dc34
Marcus Rejås <marcus.rejas@alleato.se>
2020-05-20 13:23:35 +0200

    Added first version

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # signalk-iotopen-lynx-gw
-Signalk to IoT Open Lynx Gateway
+
+Signalk to IoT Open Lynx Gateway is a simple way to get data from your boat
+running [Signalk Node Server](https://signalk.org) to the IoT platform Lynx by
+[IoT Open](https://iotopen.se). By doing this you can store and analyze the
+data using all the tools available for Lynx, e.g. displaying data with Grafana
+or react on events with node-red. It is even possible to turn on a lamp or
+interact with your smart home from the boat. One example is to flash a light if
+your boat leaves the dock. There are lots of possibilites and it is fun!
+
+## Features
+
+* Exports chosen paths to Lynx
+* Creates the functions in Lynx automatically
+
+## Usage
+
+Install from the Appstore in Signal K. Go to Server -> Plugin Config and configure it. From Lynx you will need the following information:
+
+* Hostname
+* Mqtt port
+* API Key
+* Installation ID
+* Client ID
+
+You can get all this from your Lynx account.
+
+## Screenshots
+
+Config screen:
+
+![signalk-integration1](https://user-images.githubusercontent.com/3830271/82436494-3436cd00-9a96-11ea-86c6-347ff58b8845.png)
+
+Functions in Lynx
+
+![lynx](https://user-images.githubusercontent.com/3830271/82440643-f8533600-9a9c-11ea-982c-00ef83b2bb40.png)
+
+Graphs in Grafana
+
+![grafana](https://user-images.githubusercontent.com/3830271/82440651-fab59000-9a9c-11ea-962e-1c9943391d81.png)
---
+++ index.js
@@ -0,0 +1,294 @@
+/*
+* Copyright 2019, 2020 Marcus Rejas
+*
+* Some code and inspiration is from signalk-mqtt-gw
+* Copyright 2016 Teppo Kurki <teppo.kurki@iki.fi>
+*
+* But the mistakes are mine :-)
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+const id = 'signalk-iotopen-lynx-gw';
+const debug = require('debug')(id);
+const mqtt = require('mqtt');
+const NeDBStore = require('mqtt-nedb-store');
+const Request = require("request");
+
+var known_paths = [];
+
+function log_to_file(message) {
+	console.log("File logging disabled\n")
+}
+
+
+function lynxObjectExists(path, config) {
+	console.log('lynxObjectExists called with ' + path + '\n'); 
+
+	if (known_paths.includes(path)) {
+		console.log('Known in cache\n');
+		return;
+	} else {
+		console.log('Not known in cache\n');
+	}
+
+	var urlPrefix = '';
+	var returnValue = false;
+	if ( config.useTLS ) {
+		urlPrefix = 'https://';
+	} else {
+		urlPrefix = 'http://';
+	}
+
+	var url = urlPrefix + config.restHost + '/api/v2/functionx/' + config.installationId + '?signalk_path=' + path;
+
+
+	console.log('token:' + config.password);
+
+	var options = {
+		uri: url,
+		method: 'GET',
+		headers: { 'User-Agent': 'SignalK IoT Open Gateway' }
+	};
+
+	Request(options, function (error, response, body) {
+		if (error) {
+			console.log('Error cought' + error + '\n');
+		}
+
+		var result = [ "undefined" ];		
+
+    		try {
+			result = JSON.parse(body);
+    		} catch(e) {
+        		console.log(e);
+    		}
+
+		console.log(body);
+
+		if (typeof result[0] != "undefined" && path.localeCompare(result[0].meta.signalk_path) == 0) {
+			console.log(path + '==' + result[0].meta.signalk_path + '==> TRUE\n');
+			console.log('Adding to cache\n');
+			known_paths.push(path);
+		} else {
+			createLynxObject(path, config);
+		}
+	}).auth('token', config.password, true);
+}
+
+function createLynxObject(path, config) {
+	console.log('createLynxObject called with ' + path +  '\n'); 
+	var urlPrefix = '';
+	if ( config.useTLS ) {
+	urlPrefix = 'https://';
+	} else {
+	urlPrefix = 'http://';
+	}
+	var url = urlPrefix + config.restHost + '/api/v2/functionx/' + config.installationId + '?signalk_path=' + path;
+	var topic = 'obj/signalk/' + path.replace(/\./g, '/');
+
+	var options = {
+		uri: url,
+		method: 'POST',
+		json: {
+			"installation_id": config.installationId,
+			"type": "signalk",
+			"meta": {
+				"signalk_path": path,
+				"topic_read": topic,
+				"name": path
+			},
+	 	}
+	};
+
+	Request(options, function (error, response, body) {
+		console.log(body);
+		if (error) {
+			console.log("Error createing LynxObject " + body);
+			return(false);
+		}
+
+	}).auth('token', config.password, true);
+	return(true);
+}
+
+module.exports = function(app) {
+	var plugin = {
+		unsubscribes: [],
+	};
+	var server
+
+	plugin.id = id;
+	plugin.name = 'SignalK to IoT Open Lynx (alpha)';
+	plugin.description =
+		'Plugin that provides gateway functionality from Signal K to IoT Open Lynx';
+
+	plugin.schema = {
+		title: 'Signal K - IoT Open Lynx Gateway',
+		type: 'object',
+		properties: {
+			restHost: {
+				type: 'string',
+				title: 'IoT Open Lynx REST server (e.g. lynx.iotopen.se)',
+				description:
+					'IoT Open Lynx server that the paths listed below should be sent to',
+				default: 'lynx.iotopen.se',
+			},
+			mqttHost: {
+				type: 'string',
+				title: 'IoT Open Lynx MQTT server and port (e.g. lynx.iotopen.se:8883)',
+				description:
+					'IoT Open Lynx server that the paths listed below should be sent to. In most cases the same as the REST server and port is most likely 8883.',
+				default: 'lynx.iotopen.se:8883',
+			},
+			useTLS: {
+				type: "boolean",
+				default: true,
+				title: "Use TLS encryption (https/mqtts)"
+			},
+			rejectUnauthorized: {
+				type: "boolean",
+				default: true,
+				title: "Reject self signed and invalid server certificates"
+			},
+			password: {
+				type: "string",
+				title: "Token from Lynx"
+			},
+			installationId: {
+				type: "string",
+				title: "Installation ID from Lynx"
+			},
+			clientId: {
+				type: "string",
+				title: "Client ID from Lynx"
+			},
+			paths: {
+				type: 'array',
+				title: 'Signal K self paths to send',
+				description: 'See the Data Browser to get currently known self paths',
+				//description: 'Seen paths:' + app.streambundle.getAvailablePaths().toString(), //.join(),
+				//description: app.streambundle.getAvailablePaths().join(),
+				//description: app.streambundle.getAvailablePaths(),
+				default: [{ path: 'navigation.position', interval: 60 }],
+				items: {
+					type: 'object',
+					properties: {
+						path: {
+							type: 'string',
+							title: 'Path',
+						},
+						interval: {
+							type: 'number',
+							title:
+								'Minimum interval between updates for this path to be sent to the server',
+						},
+					},
+				},
+			},
+		},
+	};
+
+	var started = false;
+
+	plugin.onStop = [];
+
+	plugin.start = function(options) {
+		plugin.onStop = [];
+		var urlPrefix = "";
+		if ( options.useTLS ) {
+			urlPrefix = 'mqtts://';
+		} else {
+			urlPrefix = 'mqtt://'
+		}
+
+		const manager = NeDBStore(app.getDataDirPath());
+		const client = mqtt.connect(urlPrefix + options.mqttHost, {
+			rejectUnauthorized: options.rejectUnauthorized,
+			reconnectPeriod: 60000,
+			clientId: app.selfId,
+			outgoingStore: manager.outgoing,
+			username: 'Token',
+			password: options.password
+		});
+		client.on('error', (err) => console.error(err))
+		startSending(options, client, plugin.onStop);
+		plugin.onStop.push(_ => client.end());
+		started = true;
+	};
+
+	plugin.stop = function() {
+		plugin.onStop.forEach(f => f());
+	};
+
+	plugin.statusMessage = function () {
+		if (started)
+			return 'Connection to IoT Open established.';
+		else
+			return 'Not yet connected to IoT Open.';
+	}
+
+	function startSending(options, client, onStop) {
+		options.paths.forEach(pathInterval => {
+			// If there are more than one value we need to split it up.
+			// If the value is not numeric we have to skip it.
+			//
+			onStop.push(
+				app.streambundle
+					.getSelfBus(pathInterval.path)
+					.debounceImmediate(pathInterval.interval * 1000)
+					.log()
+					.onValue(normalizedPathValue => {
+						console.log('Got Value = ' + normalizedPathValue.value);
+						console.log('Got Unit = ' + normalizedPathValue.unit);
+						console.log("Checking if path exists: " + pathInterval.path);
+						
+						var path_list = {};
+
+						if (typeof normalizedPathValue.value === 'object') {
+							// We have to create a path for each value. Adding the full value to msg.
+							Object.keys(normalizedPathValue.value).forEach(function(index, item) {
+								const lynxMetric = { value: normalizedPathValue.value[index], msg: JSON.stringify(normalizedPathValue.value) }
+								console.log('Adding ' + normalizedPathValue.value[index] + ' to ' + pathInterval.path + '.'+ index + '\n');
+								path_list[pathInterval.path + '.'+ index] = Object.create(lynxMetric);
+							});
+
+						} else {
+							const lynxMetric = { value: normalizedPathValue.value, msg: normalizedPathValue.value.toString() }
+							console.log('Adding ' + normalizedPathValue.value + ' to ' + pathInterval.path + '\n');
+							path_list[pathInterval.path] = Object.create(lynxMetric);
+						}
+
+
+						Object.keys(path_list).forEach(function(path, value) {
+							console.log('path=' + path + ' value=' + path_list[path] + '\n');
+							lynxObjectExists(path, options);
+							client.publish(
+								options.clientId.concat('/obj/signalk/', path.replace(/\./g, '/')),
+								JSON.stringify({
+									timestamp: new Date(normalizedPathValue.timestamp).getTime() / 1000,
+									value: path_list[path].value,
+									msg: path_list[path].msg
+								}),
+								{ qos: 2 }
+						)
+						}); 
+
+					}
+					)
+			);
+		});
+	}
+
+	return plugin;
+};
